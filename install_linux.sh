b6547a919b1066bdd5812bf525d336c03801459d
sotokisehiro <sotokisehiro@gmail.com>
2023-08-20 04:40:51 +0900

    簡易インストーラ（Linux用）の追加

---
+++ install_linux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# 最小限必要なLinuxコマンドのインストール（管理者権限が必要）
+sudo apt install git
+sudo apt install pip
+sudo apt install python3-venv
+
+# ダウンロード＆インストール
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+ct2-transformers-converter --model line-corporation/japanese-large-lm-3.6b-instruction-sft --low_cpu_mem_usage --output_dir line-sft --quantization int8 --force
+
+# サーバー起動
+python3 ct2-main.py
