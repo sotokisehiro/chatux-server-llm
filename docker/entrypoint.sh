9b347412388745fce6459de8c7b795852a8112ae
TAKANO Mitsuhiro <takano32@gmail.com>
2023-09-11 23:43:58 +0900

    fix command line option

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,5 @@ git fetch origin
 git switch main
 git pull origin main
 
-python3 main.py --aiengine=0 --listen="0.0.0.0" --maxspeed ON
-
+python3 main.py --aiengine=0 --listen="0.0.0.0" --maxspeed=ON
 
