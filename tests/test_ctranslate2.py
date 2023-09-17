fcb9d803f67cca0acd01058126244843b130c0d9
TAKANO Mitsuhiro <takano32@gmail.com>
2023-09-18 01:28:55 +0900

    pytest

---
+++ tests/test_ctranslate2.py
@@ -0,0 +1,16 @@
+import os
+
+import pytest
+
+from engine import CTranslate2Engine
+
+
+class TestCTranslate2Engine:
+    @pytest.fixture
+    def init_engine(self):
+        self.engine = CTranslate2Engine(os.cpu_count())
+
+    def test_self_introduction(self, init_engine):
+        res = self.engine.generate_text("自己紹介してください。")
+        print("res: {res}")
+        assert "アシスタント" in res
