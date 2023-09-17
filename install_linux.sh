d3c95690797eb5a24bc9da3fe73db9d6a5cf4164
Sehiro Sotoki <sotokisehiro@gmail.com>
2023-09-17 09:34:02 +0900

    ELYZAモデルの自動ダウンロードを追加。

--- install_linux.sh
+++ install_linux.sh
@@ -10,6 +10,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 ct2-transformers-converter --model line-corporation/japanese-large-lm-3.6b-instruction-sft --low_cpu_mem_usage --output_dir line-sft --quantization int8 --force
+wget -P models https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-7b-fast-instruct-gguf/resolve/main/ELYZA-japanese-Llama-2-7b-fast-instruct-q4_K_M.gguf
 
 # サーバー起動
 python3 main.py
