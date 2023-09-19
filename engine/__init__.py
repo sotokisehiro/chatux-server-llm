b0c8989f0c283f5b9830691e1eaa22fa3504ab83
TAKANO Mitsuhiro <takano32@gmail.com>
2023-09-19 13:15:09 +0900

    disable mypy check in temporary

--- engine/__init__.py
+++ engine/__init__.py
@@ -2,4 +2,4 @@ from engine.ctranslate2 import CTranslate2Engine
 from engine.engine import Engine
 from engine.llama_cpp import LlamaCppEngine
 
-__all__ = [Engine, LlamaCppEngine, CTranslate2Engine]
+__all__ = ["Engine", "LlamaCppEngine", "CTranslate2Engine"]
